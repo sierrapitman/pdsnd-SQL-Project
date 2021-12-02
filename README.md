1d579fb943fbc6c1053336e33912f8ef64ef3459
Sierra Pitman <sierra.rai@gmail.com>
2021-12-02 12:23:20 +0100

    chore: Build initial repository

---
+++ README.md
@@ -0,0 +1,30 @@
+### Date created
+July 21, 2021
+
+### Project Title
+SQL Project: Investigate a Relational Database: Sakila DVD Rental Database
+
+### Description
+"In this project, you will query the Sakila DVD Rental database. The Sakila Database holds information about a company that rents movie DVDs. For this project, you will be querying the database to gain an understanding of the customer base, such as what the patterns in movie watching are across different customer groups, how they compare on payment earnings, and how the stores compare in their performance. To assist you in the queries ahead, the schema for the DVD Rental database is provided below.
+
+Project Submissions:
+Presentations
+You are now on the portion of the project you will need to submit to a reviewer. To pass this project follow the instructions below to create a presentation. You will submit a zip file with two items:
+
+Slide Deck (4 slides):
+Text File with SQL queries
+Your presentation should include:
+
+Four slides:
+One question on each slide
+One visualization (graph / chart / table) per slide
+A 1-2 sentence answer to the question, based on the data and visualization, on each slide
+Indicate which code in the attached text file was used to create the visualization OR add the query used to generate the visualization" (Udacity)
+
+### Files used & Resources
+dvd-rental-erd.pdf
+https://www.postgresqltutorial.com/postgresql-sample-database/ : DVD Rental Sample Database
+UDacity-SQL-Project.sql
+
+### Credits
+Project Description provided by Udacity.
---
+++ UDacity-SQL-Project.sql
@@ -0,0 +1,101 @@
+--Set 1 Q1: 
+--Create a query that lists each movie, the film category it is
+--classified in, and the number of times it has been rented
+--out.
+
+SELECT
+  sub.category AS category
+  , SUM(rental_count) AS rental_count
+FROM (
+    SELECT DISTINCT f.title AS title
+      , c.name as category
+      , COUNT(r.rental_id) OVER (PARTITION BY f.title) AS rental_count
+      FROM film f
+      JOIN film_category fc
+        ON f.film_id = fc.film_id
+      JOIN category c
+        ON fc.category_id = c.category_id
+      JOIN inventory i
+        ON f.film_id = i.film_id
+      JOIN rental r
+        ON i.inventory_id = r.inventory_id
+    WHERE c.name in ('Animation', 'Children', 'Classics', 'Comedy', 'Family', 'Music')) sub
+    GROUP BY 1
+    ORDER BY 2;
+
+
+
+--Set 1 q2:
+--Can you provide a table with the movie titles and divide
+--them into 4 levels (first_quarter, second_quarter,
+--third_quarter, and final_quarter) based on the quartiles
+--(25%, 50%, 75%) of the rental duration for movies across
+--all categories?
+
+SELECT sub.title AS title
+      , sub.category AS category
+      , sub.rental_duration AS rental_duration
+      , NTILE(4) OVER (PARTITION BY sub.rental_duration) AS quartile
+  FROM
+    (SELECT
+      f.title AS title
+      , c.name AS category
+      , f.rental_duration AS rental_duration
+    FROM film f
+    JOIN film_category fc
+      ON f.film_id = fc.film_id
+    JOIN category c
+      ON fc.category_id = c.category_id
+    WHERE c.name in ('Animation', 'Children', 'Classics', 'Comedy', 'Family', 'Music')) AS sub
+ORDER BY 4, 3, 2, 1;
+
+
+
+--Set 1 q3:
+--Finally, provide a table with the family friendly film
+--category, each of the quartiles, and the corresponding
+--count of movies within each combination of film category
+--for each corresponding rental duration category. The
+--resulting table should have three columns: Category, Rental length category, Count
+
+SELECT category, quartile, count(quartile)
+FROM(
+  SELECT sub.title AS title
+        , sub.category AS category
+        , sub.rental_duration AS rental_duration
+        , NTILE(4) OVER (PARTITION BY sub.rental_duration) AS quartile
+  FROM
+    (SELECT
+      f.title AS title
+      , c.name AS category
+      , f.rental_duration AS rental_duration
+    FROM film f
+    JOIN film_category fc
+      ON f.film_id = fc.film_id
+    JOIN category c
+      ON fc.category_id = c.category_id
+    WHERE c.name in ('Animation', 'Children', 'Classics', 'Comedy', 'Family', 'Music')) AS sub) sub2
+GROUP BY 1, 2
+ORDER BY 1, 2;
+
+
+
+--Set 2 q1:
+--Write a query that returns the store ID for the store, the
+--year and month and the number of rental orders each
+--store has fulfilled for that month. Your table should
+--include a column for each of the following: year, month,
+--store ID and count of rental orders fulfilled during that
+--month.
+
+SELECT s.store_id
+  , date_part ('month', r.rental_date) AS rental_month
+  , date_part ('year', r.rental_date) AS rental_year
+  , count(r.rental_id)
+FROM rental r
+JOIN staff st
+ON r.staff_id = st.staff_id
+JOIN store s
+ON st.store_id = s.store_id
+GROUP BY 1, 2, 3
+ORDER BY 3, 2, 1;
